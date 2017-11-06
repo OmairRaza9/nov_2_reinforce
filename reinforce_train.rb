09d83a69337fc4ce49fd78db8e5e45dea6b3c86e
Omair <omairraza9@gmail.com>
2017-11-06 10:10:23 -0500

    finished part 2

---
+++ reinforce_train.rb
@@ -0,0 +1,27 @@
+ train_station = [
+{train: "72C", frequency_in_minutes: 15, direction: "north"},
+{train: "72D", frequency_in_minutes: 15, direction: "south"},
+{train: "610", frequency_in_minutes: 5, direction: "north"},
+{train: "611", frequency_in_minutes: 5, direction: "south"},
+{train: "80A", frequency_in_minutes: 30, direction: "east"},
+{train: "80B", frequency_in_minutes: 30, direction: "west"},
+{train: "110", frequency_in_minutes: 15, direction: "north"},
+{train: "111", frequency_in_minutes: 15, direction: "south"}
+]
+array = []
+train_station.each do |value|
+  if value[:train] == "111"
+    array << value[:direction]
+  end
+end
+p array
+
+array_two = []
+train_station.each do |two|
+  if two[:train] == "80B"
+    array_two << two[:frequency_in_minutes]
+
+  end
+end
+
+p array_two
